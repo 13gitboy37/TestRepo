fa0c21787814ff6f703887276025074f8f482bb6
13gitboy37 <trane0909@bk.ru>
2022-09-04 14:43:12 +0300

    Initialize project

---
+++ TestRepoForGit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TestRepoForGit
+//
+//  Created by Никита Мошенцев on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
